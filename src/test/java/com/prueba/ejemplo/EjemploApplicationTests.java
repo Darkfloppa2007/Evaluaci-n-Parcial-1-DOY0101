ac7c79c9909ae175b9a96a73eb00b49ff3a66ba9
Darkfloppa2007 <mat.carrascoi@duocuc.cl>
2026-06-25 15:04:00 -0400

    commit ?

--- src/test/java/com/prueba/ejemplo/EjemploApplicationTests.java
+++ src/test/java/com/prueba/ejemplo/EjemploApplicationTests.java
@@ -3,7 +3,7 @@ package com.prueba.ejemplo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Ejemplo.class)
 class EjemploApplicationTests {
 
 	@Test
